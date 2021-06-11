9551453ff4c693cbab1d720d32586c80c5898fe3
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2021-06-11 17:52:26 +0300

    15. Posts, post component; folder structure

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./MyPosts.module.css";
+import Post from "./Post/Post";
+
+const MyPosts = () => {
+    return (
+        <div>
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+
+            </div>
+            <div className={styles.posts}>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+            </div>
+        </div>
+    )
+
+}
+
+export default MyPosts;
