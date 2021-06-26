49e177f12633fe259e8f9967429fb3460d1bdd33
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2021-06-26 14:45:57 +0300

    44. Context API

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import {addMessageActionCreator, updateNewMessageTextActionCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../StoreContext";
 
-const DialogsContainer = (props) => {
-    let state = props.store.getState();
+const DialogsContainer = (props) => (
+    <StoreContext.Consumer>
+        {
+            (store) => {
+                let state = store.getState().dialogsPage;
 
-    let addMessage = () => {
-        props.store.dispatch(addMessageActionCreator());
-    }
+                let addMessage = () => store.dispatch(addMessageActionCreator());
 
-    let onMessageChange = (text) => {
-        props.store.dispatch(updateNewMessageTextActionCreator(text));
-    }
+                let onMessageChange = (text) => store.dispatch(updateNewMessageTextActionCreator(text));
 
-    return (
-        <Dialogs updateNewMessageText={onMessageChange}
-                 addMessage={addMessage}
-                 dialogs={state.dialogsPage.dialogs}
-                 messages={state.dialogsPage.messages}
-                 newMessageText={state.dialogsPage.newMessageText}/>
-    )
-}
+                return (
+                    <Dialogs updateNewMessageText={onMessageChange}
+                             addMessage={addMessage}
+                             dialogs={state.dialogs}
+                             messages={state.messages}
+                             newMessageText={state.newMessageText}/>
+                )
+            }
+        }
+    </StoreContext.Consumer>
+)
 
 export default DialogsContainer;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import './index.css';
 import store from "./redux/redux-store";
+import StoreContext from "./components/StoreContext";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
+                <StoreContext.Provider value={store}>
+                    <App/>
+                </StoreContext.Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
