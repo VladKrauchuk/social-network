1a0331edba52ebb8692d79f809fa053bbb21b2ec
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2021-07-08 19:04:15 +0300

    55. Pagination for users page

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,15 +1,20 @@
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => ({
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
 })
 
 let mapDispatchToProps = (dispatch) => ({
     follow: (userId) => dispatch(followAC(userId)),
     unfollow: (userId) => dispatch(unfollowAC(userId)),
     setUsers: (users) => dispatch(setUsersAC(users)),
+    setCurrentPage: (pageNumber) => dispatch(setCurrentPageAC(pageNumber)),
+    setTotalUsersCount: (totalUsersCount) => dispatch(setTotalUsersCountAC(totalUsersCount)),
 })
 
 const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
