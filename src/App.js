4be48efac96b838448b7f42f180ca8df5b5c86e3
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2021-07-16 15:51:09 +0300

    61. Auth/me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import {Route} from "react-router-dom";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
@@ -9,11 +8,12 @@ import News from "./components/News/News";
 import UsersContainer from "./components/Users/UsersContainer";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
     return (
         <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Route path='/dialogs'
